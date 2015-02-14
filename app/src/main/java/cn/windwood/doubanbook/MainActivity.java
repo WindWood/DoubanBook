4b91cdfe852645183282a1dd55fa98dad2056a1b
WindWood <windwood@gmail.com>
2015-02-14 12:59:27 +0800

    first app

--- app/src/main/java/cn/windwood/doubanbook/MainActivity.java
+++ app/src/main/java/cn/windwood/doubanbook/MainActivity.java
@@ -1,15 +1,13 @@
 package cn.windwood.doubanbook;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -20,7 +18,7 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new BookFragment())
                     .commit();
         }
     }
